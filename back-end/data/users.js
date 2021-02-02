66dc2e22ba2cae7734c4caea72c8734967d04e4a
Hillary <nwanjiuche@gmail.com>
2021-02-02 12:46:04 +0100

    second commit

---
+++ back-end/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: ture,
+  },
+  {
+    name: "Uche User",
+    email: "uche@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Faith User",
+    email: "faith@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
